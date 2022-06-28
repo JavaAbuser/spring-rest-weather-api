f6e88f36827886bfc82a8ebb6e86cf9a8aed3a13
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-28 12:57:49 +0300

    added convertToSensor method using modelMapper and added sensor validation

--- src/main/java/com/javaabuser/restapi/controllers/SensorsController.java
+++ src/main/java/com/javaabuser/restapi/controllers/SensorsController.java
@@ -3,24 +3,41 @@ package com.javaabuser.restapi.controllers;
 import com.javaabuser.restapi.DTO.SensorDTO;
 import com.javaabuser.restapi.exceptions.sensor.ErrorResponse;
 import com.javaabuser.restapi.exceptions.sensor.NotCreatedException;
+import com.javaabuser.restapi.models.Sensor;
+import com.javaabuser.restapi.services.SensorsService;
+import com.javaabuser.restapi.util.SensorValidator;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
 
-@RestController("/sensor")
+@RestController
+@RequestMapping("/sensors")
 public class SensorsController {
+    private final SensorsService sensorsService;
+    private final ModelMapper modelMapper;
+    private final SensorValidator sensorValidator;
+
+    @Autowired
+    public SensorsController(SensorsService sensorsService, ModelMapper modelMapper, SensorValidator sensorValidator) {
+        this.sensorsService = sensorsService;
+        this.modelMapper = modelMapper;
+        this.sensorValidator = sensorValidator;
+    }
+
     @PostMapping("/registration")
     public ResponseEntity<HttpStatus> register(@RequestBody @Valid SensorDTO sensorDTO,
                                                BindingResult bindingResult){
+        Sensor sensor = convertToSensor(sensorDTO);
+        sensorValidator.validate(sensor, bindingResult);
+
         if(bindingResult.hasErrors()){
             StringBuilder errorsMessage = new StringBuilder();
             List<FieldError> errors = bindingResult.getFieldErrors();
@@ -35,8 +52,12 @@ public class SensorsController {
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
+    private Sensor convertToSensor(SensorDTO sensorDTO){
+        return modelMapper.map(sensorDTO, Sensor.class);
+    }
+
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleNotCreatedException(NotCreatedException exception){
+    private ResponseEntity<ErrorResponse> handleNotCreatedException(NotCreatedException exception){
         ErrorResponse errorResponse = new ErrorResponse(exception.getMessage(), new Date());
         return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
     }
