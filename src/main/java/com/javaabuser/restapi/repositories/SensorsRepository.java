0caa725aaf573957f2e0ab8c5c23b806b237ef13
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-29 01:57:38 +0300

    added findByName method

--- src/main/java/com/javaabuser/restapi/repositories/SensorsRepository.java
+++ src/main/java/com/javaabuser/restapi/repositories/SensorsRepository.java
@@ -4,6 +4,9 @@ import com.javaabuser.restapi.models.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SensorsRepository extends JpaRepository<Sensor, Integer> {
+    public Optional<Sensor> findByName(String sensorName);
 }
