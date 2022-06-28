90fa458896bd01d160d59198b48a3f6355e698a0
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-29 01:58:22 +0300

    initial commit

---
+++ src/main/java/com/javaabuser/restapi/util/SensorValidator.java
@@ -0,0 +1,31 @@
+package com.javaabuser.restapi.util;
+
+import com.javaabuser.restapi.models.Sensor;
+import com.javaabuser.restapi.services.SensorsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class SensorValidator implements Validator {
+    private final SensorsService sensorsService;
+
+    @Autowired
+    public SensorValidator(SensorsService sensorsService) {
+        this.sensorsService = sensorsService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Sensor.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Sensor sensor = (Sensor) target;
+        if(sensorsService.findByName(sensor.getName()).isEmpty()){
+            errors.rejectValue("name", "", "Sensor with this name already exists.");
+        }
+    }
+}
