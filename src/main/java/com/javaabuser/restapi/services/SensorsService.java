a286b63ba301ce0d980ca05f797bf5baf112fd90
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-29 01:58:08 +0300

    added findByName, findAll and save methods

--- src/main/java/com/javaabuser/restapi/services/SensorsService.java
+++ src/main/java/com/javaabuser/restapi/services/SensorsService.java
@@ -1,9 +1,13 @@
 package com.javaabuser.restapi.services;
 
+import com.javaabuser.restapi.models.Sensor;
 import com.javaabuser.restapi.repositories.SensorsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -13,5 +17,18 @@ public class SensorsService {
     @Autowired
     public SensorsService(SensorsRepository sensorsRepository) {
         this.sensorsRepository = sensorsRepository;
+    }
+
+    public List<Sensor> findAll(){
+        return sensorsRepository.findAll();
+    }
+
+    public Optional<Sensor> findByName(String sensorName){
+        return sensorsRepository.findByName(sensorName);
+    }
+
+    @Transactional
+    public void save(Sensor sensor){
+        sensorsRepository.save(sensor);
     }
 }
