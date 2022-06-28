c3e001742d4c5a92558eb0a9449a02f0cb21a985
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-29 01:53:52 +0300

    added findAll method

--- src/main/java/com/javaabuser/restapi/services/MeasurementsService.java
+++ src/main/java/com/javaabuser/restapi/services/MeasurementsService.java
@@ -1,9 +1,12 @@
 package com.javaabuser.restapi.services;
 
+import com.javaabuser.restapi.models.Measurement;
 import com.javaabuser.restapi.repositories.MeasurementsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -13,5 +16,14 @@ public class MeasurementsService {
     @Autowired
     public MeasurementsService(MeasurementsRepository measurementsRepository) {
         this.measurementsRepository = measurementsRepository;
+    }
+
+    public List<Measurement> findAll(){
+        return measurementsRepository.findAll();
+    }
+
+    @Transactional
+    public void save(Measurement measurement) {
+        measurementsRepository.save(measurement);
     }
 }
