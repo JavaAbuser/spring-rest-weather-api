84ea3cbd27812e571dc4e87bd63216bfb4b6bcd2
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-29 01:53:12 +0300

    added all args constructor

--- src/main/java/com/javaabuser/restapi/exceptions/sensor/ErrorResponse.java
+++ src/main/java/com/javaabuser/restapi/exceptions/sensor/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.javaabuser.restapi.exceptions.sensor;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,12 +8,8 @@ import java.util.Date;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ErrorResponse {
     private String message;
     private Date timestamp;
-
-    public ErrorResponse(String message, Date timestamp) {
-        this.message = message;
-        this.timestamp = timestamp;
-    }
 }
