330636b1fb8bc7148e80668a073e58bac9668500
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-30 01:40:26 +0300

    deleted NotCreatedException.java

--- src/main/java/com/javaabuser/restapi/exceptions/measurement/NotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package com.javaabuser.restapi.exceptions.measurement;
-
-public class NotCreatedException extends RuntimeException {
-    public NotCreatedException(String message) {
-        super(message);
-    }
-}
