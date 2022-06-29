76926fbc2dc6d238b174ec427438848e7ba42af6
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-29 13:39:16 +0300

    added sensorsRepository and enrichMeasurement method

--- src/main/java/com/javaabuser/restapi/services/MeasurementsService.java
+++ src/main/java/com/javaabuser/restapi/services/MeasurementsService.java
@@ -2,20 +2,24 @@ package com.javaabuser.restapi.services;
 
 import com.javaabuser.restapi.models.Measurement;
 import com.javaabuser.restapi.repositories.MeasurementsRepository;
+import com.javaabuser.restapi.repositories.SensorsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 public class MeasurementsService {
     private final MeasurementsRepository measurementsRepository;
+    private final SensorsRepository sensorsRepository;
 
     @Autowired
-    public MeasurementsService(MeasurementsRepository measurementsRepository) {
+    public MeasurementsService(MeasurementsRepository measurementsRepository, SensorsRepository sensorsRepository) {
         this.measurementsRepository = measurementsRepository;
+        this.sensorsRepository = sensorsRepository;
     }
 
     public List<Measurement> findAll(){
@@ -24,6 +28,12 @@ public class MeasurementsService {
 
     @Transactional
     public void save(Measurement measurement) {
+        enrichMeasurement(measurement);
         measurementsRepository.save(measurement);
+    }
+
+    public void enrichMeasurement(Measurement measurement){
+        measurement.setSensor(sensorsRepository.findByName(measurement.getSensor().getName()).get());
+        measurement.setCreatedAt(new Date());
     }
 }
