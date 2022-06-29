236621faef3e698b8f5b4670008533c74ed6bd7c
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-30 01:40:38 +0300

    deleted NotFoundException.java

--- src/main/java/com/javaabuser/restapi/exceptions/measurement/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.javaabuser.restapi.exceptions.measurement;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
